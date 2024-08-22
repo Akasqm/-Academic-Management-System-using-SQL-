6300ebf62522f359b0c1250ba1c4c30a44514d07
Akasqm <119439982+Akasqm@users.noreply.github.com>
2024-08-22 08:36:04 +0530

    Add files via upload

---
+++ b) Write a query to retrieve a list of courses in which a specific student is enrolled.sql
@@ -0,0 +1,12 @@
+-- b) Write a query to retrieve a list of courses in which a specific student is enrolled. 
+SELECT 
+    s.STU_NAME,
+    c.COURSE_NAME
+   
+FROM 
+    enrollmentinfo e
+    JOIN studentinfo s ON s.STU_ID = e.STU_ID
+    JOIN coursesinfo c ON c.COURSE_ID = e.COURSE_ID
+    WHERE
+    s.STU_ID = 1
+    AND e.ENROLL_STATUS = 'Enrolled';
