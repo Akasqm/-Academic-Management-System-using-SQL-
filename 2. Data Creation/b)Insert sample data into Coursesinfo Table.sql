0f32a124b721bc37246297213fcbddedfe8aa872
Akasqm <119439982+Akasqm@users.noreply.github.com>
2024-08-23 22:03:57 +0530

    Add files via upload

---
+++ 2. Data Creation/b)Insert sample data into Coursesinfo Table.sql
@@ -0,0 +1,7 @@
+INSERT INTO coursesinfo(
+COURSE_ID,COURSE_NAME,COURSE_INSTRUCTOR_NAME)
+VALUES(1, 'Introduction to Data Science', 'Prof. Rakesh'),
+    (2, 'Data Structures', 'Prof. Meera'),
+    (3, 'Database Management Systems', 'Prof. Rose');
+    
+  
