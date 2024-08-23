bd27480b73873d17dcca92ce65f96beef03e95ee
Akasqm <119439982+Akasqm@users.noreply.github.com>
2024-08-23 12:54:45 +0530

    Add files via upload

---
+++ 4. Reporting and Analytics (Using joining queries)/d) list of students who are enrolled in multiple courses.sql
@@ -0,0 +1,13 @@
+-- d) Write a query to retrieve the list of students who are enrolled in multiple courses 
+SELECT 
+studentinfo.STU_ID,
+studentinfo.STU_NAME,
+COUNT(enrollmentinfo.COURSE_ID)
+FROM studentinfo JOIN enrollmentinfo 
+ON studentinfo.STU_ID = enrollmentinfo.STU_ID
+GROUP BY studentinfo.STU_ID,
+studentinfo.STU_NAME
+HAVING COUNT(enrollmentinfo.COURSE_ID) > 1
+;
+
+
