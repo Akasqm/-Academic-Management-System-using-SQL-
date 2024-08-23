e54e81e340ba092bbcffe6656dd6b670e22e6c07
Akasqm <119439982+Akasqm@users.noreply.github.com>
2024-08-23 21:57:25 +0530

    Delete CoursesInfo table/Coursesinfo Table with data.sql

--- CoursesInfo table/Coursesinfo Table with data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO coursesinfo(
-COURSE_ID,COURSE_NAME,COURSE_INSTRUCTOR_NAME)
-VALUES(1, 'Introduction to Data Science', 'Prof. Rakesh'),
-    (2, 'Data Structures', 'Prof. Meera'),
-    (3, 'Database Management Systems', 'Prof. Rose');
-    
-  
