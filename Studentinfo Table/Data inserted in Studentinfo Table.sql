03077f97b89ec67c32529359eb3d0a0c49e52515
Akasqm <119439982+Akasqm@users.noreply.github.com>
2024-08-26 12:16:32 +0530

    Delete Studentinfo Table directory

--- Studentinfo Table/Data inserted in Studentinfo Table.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Studentinfo (
-STU_ID,STU_NAME,DOB,PHONE_NO,EMAIL_ID,ADDRESS)
-VALUES (1, 'AKASH', '1990-12-30', '8089686666', 'a123@gmail.com', 'Kannur, India'),
-(2, 'AKSHAYA', '1990-12-30', '8089686667', 'a124@gmail.com', 'Kochi, India'),
-(3, 'DHYAN', '1990-12-30', '8089686668', 'a125@gmail.com', 'Kannur, India');
