530cc5567399ce675c23cd325c5dd323168c37ab
Akasqm <119439982+Akasqm@users.noreply.github.com>
2024-08-28 20:14:12 +0530

    Update c) Write a query to retrieve course information, including course name, instructor information.sql

--- 3.Retrieve the Student Information/c) Write a query to retrieve course information, including course name, instructor information.sql
+++ 3.Retrieve the Student Information/c) Write a query to retrieve course information, including course name, instructor information.sql
@@ -1,3 +1,3 @@
-/* c) Write a query to retrieve course information, including course name, instructor information. 
+-- c) Write a query to retrieve course information, including course name, instructor information. 
 SELECT COURSE_NAME,COURSE_INSTRUCTOR_NAME
 FROM Coursesinfo;
