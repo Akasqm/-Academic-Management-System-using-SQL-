fd898bd457d265d6ed6adf8a2007a4a691901f2f
Akasqm <119439982+Akasqm@users.noreply.github.com>
2024-08-28 20:21:01 +0530

    Update a) Write a query to retrieve student details, such as student name, contact informations, and Enrollment status.sql

--- 3.Retrieve the Student Information/a) Write a query to retrieve student details, such as student name, contact informations, and Enrollment status.sql
+++ 3.Retrieve the Student Information/a) Write a query to retrieve student details, such as student name, contact informations, and Enrollment status.sql
@@ -3,6 +3,7 @@ SELECT
     STU_NAME,
     PHONE_NO,
     EMAIL_ID,
+    ADDRESS,
     CASE WHEN ENROLLMENT_STATUS = 1 THEN 'Enrolled' ELSE 'Not Enrolled' END AS ENROLLMENT_STATUS
 FROM
     Studentinfo;
